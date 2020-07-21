94aa8f8bee9d8f0096d036d613281d9b37d7c8df
Ryan Andri <ryan-andri@outlook.com>
2020-07-21 08:57:33 +0700

    ASoC: msm: qdsp6v2: fix export symbol of version checking
    Fixes: 36e58bc ("ASoC: msm: qdsp6v2: Make version checking no-op")
    Change-Id: I9bd987288b873ccf894af00deaed02892359b285

--- sound/soc/msm/qdsp6v2/q6core.c
+++ sound/soc/msm/qdsp6v2/q6core.c
@@ -444,7 +444,6 @@ int q6core_get_service_version(uint32_t service_id,
 	pr_err("%s: No service matching service ID %d\n", __func__, service_id);
 	return -EINVAL;
 }
-EXPORT_SYMBOL(q6core_get_service_version);
 
 size_t q6core_get_fwk_version_size(uint32_t service_id)
 {
@@ -501,8 +500,6 @@ size_t q6core_get_fwk_version_size(uint32_t service_id)
 done:
 	return ret;
 }
-EXPORT_SYMBOL(q6core_get_fwk_version_size);
-
 #else
 int q6core_get_service_version(uint32_t service_id,
 			       struct avcs_fwk_ver_info *ver_info,
@@ -510,6 +507,9 @@ int q6core_get_service_version(uint32_t service_id,
 size_t q6core_get_fwk_version_size(uint32_t service_id)
 	{ return -1; }
 #endif
+EXPORT_SYMBOL(q6core_get_service_version);
+EXPORT_SYMBOL(q6core_get_fwk_version_size);
+
 int32_t core_set_license(uint32_t key, uint32_t module_id)
 {
 	struct avcs_cmd_set_license *cmd_setl = NULL;
